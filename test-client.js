670109c1b85a2294617672afaf96123e5fd3c84d
Gavin Beretvas <beretvag@oregonstate.edu>
2026-08-10 15:20:18 -0700

    Simplify demo client

--- test-client.js
+++ test-client.js
@@ -1,3 +1,2 @@
-const response = await fetch('http://127.0.0.1:3050/playlist?limit=3')
-
-console.log(JSON.stringify(await response.json(), null, 2))
+const response = await fetch('http://127.0.0.1:3050/playlist')
+console.log(await response.text())
